21a3e5f6f9c956cb0c99b6ea0b7e9c8ac4d3deb5
Troy Hoath <troy.hoath@gmail.com>
2026-03-08 13:29:55 +1100

    Add global counter with Redis

--- app/api/counter/route.tsx
+++ app/api/counter/route.tsx
@@ -1,10 +1,13 @@
-let count = 0;
+import { Redis } from "@upstash/redis";
+
+const redis = Redis.fromEnv();
 
 export async function GET() {
+  const count = (await redis.get("count")) ?? 0;
   return Response.json({ count });
 }
 
 export async function POST() {
-  count += 1;
+  const count = await redis.incr("count");
   return Response.json({ count });
 }
